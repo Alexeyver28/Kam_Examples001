a65f11df7e81674812160ad4dce3553ea5fb35af
Алексей Верещагин <aleksejveresagin@MacBook-Pro-Aleksej.local>
2022-12-19 01:30:03 +0500

    add Perfomance 003

---
+++ Perfomance003/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Массив из n элементов, требуется найти элемент find
+// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if (arg2 > result) result = arg2;
+//     if (arg3 > result) result = arg3;
+//     return result;
+// }
+int[] array = { 2, 3, 4, 676, 6, 7, 8, 9, 1, 4 };
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+ if (array[index] == find)
+ {
+    Console.WriteLine(index);
+    break;
+ }   
+ // index = index +1 , тоже самое, что и  index++;
+ index++;
+}
+
